33e8a2f6eba09e8b70fb705ec145ba8c9cf08b36
Layne Moseley <lmoseley@instructure.com>
2017-10-09 11:31:18 -0600

    createReactClass

--- SearchBar.js
+++ SearchBar.js
@@ -8,9 +8,11 @@ RNSearchBar = ReactNative.requireNativeComponent('RNSearchBar', null);
 
 PropTypes = require('prop-types')
 
+var createReactClass = require('create-react-class');
+
 NativeModules = ReactNative.NativeModules;
 
-SearchBar = React.createClass({
+SearchBar = createReactClass({
   propTypes: {
     placeholder: PropTypes.string,
     text: PropTypes.string,
